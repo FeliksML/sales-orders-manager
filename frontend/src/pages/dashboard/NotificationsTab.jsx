41c03f4335fd208e4e112f7c552363fe2a82a64c
FeliksML <feliks.torosyan@icloud.com>
2025-12-04 23:18:04 -0800

    fix: icon size fix N 2

--- frontend/src/pages/dashboard/NotificationsTab.jsx
+++ frontend/src/pages/dashboard/NotificationsTab.jsx
@@ -108,7 +108,7 @@ function NotificationsTab() {
       case 'followup_due':
         return '📞'
       default:
-        return <img src={bellIcon} alt="Notification" className="w-12 h-12 object-contain" />
+        return <img src={bellIcon} alt="Notification" className="w-24 h-24 object-contain" />
     }
   }
 
